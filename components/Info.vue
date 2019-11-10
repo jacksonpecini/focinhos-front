87919a07ca24e8e069ad55f2046b38044d36934a
Jack Pecini <jacksonpecini10@gmail.com>
2019-11-10 18:59:50 -0300

    fazendo faq

---
+++ components/Info.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="info">
+      <div class="info__content">
+    
+          <slot/>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ pages/faq.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="faq">
+    <Header />
+    <main class="main">
+        <Titulo texto="FAQ" />
+    </main>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/common/Header.vue';
+import Titulo from '@/components/Titulo.vue';
+export default {
+    components: {
+        Header,
+        Titulo,
+    }
+}
+</script>
+
+<style>
+
+.main{
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: var(--color-background);
+    min-height: 100vh;
+}
+
+.titulo{
+    margin:40px 0;
+}
+
+</style>
