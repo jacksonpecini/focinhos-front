cfd524b54ea87a809e957d34d5b637a51f977067
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2019-11-10 19:13:32 -0300

    Created info component

--- components/Info.vue
+++ components/Info.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="info">
       <div class="info__content">
-    
           <slot/>
       </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+
+};
 </script>
 
 <style>
+
+.info {
+  background-color: var(--color-primary);
+  width: 500px;
+  padding: 20px;
+  display: flex;
+  border-radius: 25px;
+}
+
+.info__content {
+  background-color: var(--color-grey);
+  width: 100%;
+  padding: 20px;
+  font-size: 20px;
+  color: #fff;
+  text-align: center;
+  border-radius: 25px;
+}
+
 </style>
--- pages/faq.vue
+++ pages/faq.vue
@@ -3,6 +3,31 @@
     <Header />
     <main class="main">
         <Titulo texto="FAQ" />
+        <Info>
+            <div class="faq__content">
+                <h3 class="faq__title">
+                    Para mais informações
+                </h3>
+                <p class="faq__text">
+                    Jack Pecini
+                </p>
+                <p class="faq__text">
+                    E-mail: XXXXXXXXXXXXXXX
+                </p>
+                <p class="faq__text">
+                    Jack Pecini
+                </p>
+                <p class="faq__text">
+                    E-mail: XXXXXXXXXXXXXXX
+                </p>
+                <p class="faq__text">
+                    Jack Pecini
+                </p>
+                <p class="faq__text">
+                    E-mail: XXXXXXXXXXXXXXX
+                </p>
+            </div>
+        </Info>
     </main>
   </div>
 </template>
@@ -10,10 +35,12 @@
 <script>
 import Header from '@/components/common/Header.vue';
 import Titulo from '@/components/Titulo.vue';
+import Info from '@/components/Info.vue';
 export default {
     components: {
         Header,
         Titulo,
+        Info,
     }
 }
 </script>
@@ -30,6 +57,19 @@ export default {
 
 .titulo{
     margin:40px 0;
+}
+
+.faq__content {
+    font-family: Roboto, sans-serif;
+}
+
+.faq__title {
+    font-size: 20px;
+}
+
+.faq__text {
+    font-size: 16px;
+    margin: 20px 0;
 }
 
 </style>
