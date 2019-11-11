4ac83dfb823a4b0b5b4eb074f4763a7dc399a1be
Jack Pecini <jacksonpecini10@gmail.com>
2019-11-11 17:15:28 -0300

    criando adotar e doar

---
+++ components/Cards.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="cards">
+      <div class="cards__content">
+          <slot/>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+.cards {
+  background-color: var(--color-primary);
+  max-width: 800px;
+  padding: 20px;
+  display: flex;
+  border-radius: 25px;
+  position: center;
+}
+
+.cards__content {
+  width: 100%;
+  padding: 20px;
+  font-size: 20px;
+  color: #fff;
+  text-align: center;
+  border-radius: 25px;
+}
+
+</style>
---
+++ pages/doar/doar.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="doar">
+    <Header />
+    <main class="main">
+        <Titulo texto="Doação"/>
+        <info>
+            <div class="adotar__content">
+
+            </div>
+        </info>
+    
+    </main>
+</div>
+  
+</template>
+
+<script>
+import Header from '@/components/common/Header.vue';
+import Titulo from '@/components/Titulo.vue';
+import Cards from '@/components/Cards.vue';
+export default {
+    components: {
+        Header,
+        Titulo,
+        Cards,
+    }
+}
+</script>
+
+<style>
+
+.main{
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: var(--color-background);
+    min-height: 100vh;
+}
+
+.titulo{
+    margin:40px 0;
+}
+
+
+
+</style>
